bb0b6a3fac947a2b679304899a4960c6dbb38321
Kyle Clemens <me@kyleclemens.com>
2017-07-30 20:40:46 -0400

    feat: add parse to IDs

--- src/commands/params/helpers/mod.rs
+++ src/commands/params/helpers/mod.rs
@@ -8,23 +8,29 @@ macro_rules! helper {
 
     #[derive(Debug, Deserialize)]
     pub struct $name {
-      #[serde(deserialize_with = "parse")]
+      #[serde(deserialize_with = "deserialize")]
       id: $kind
     }
 
-    fn parse<'de, D>(deserializer: D) -> Result<$kind, D::Error>
+    impl $name {
+      pub fn parse(s: &str) -> Result<$kind, ::std::num::ParseIntError> {
+        let mut data = None;
+        for start in $starts {
+          if s.starts_with(start) && s.ends_with('>') {
+            data = Some(&s[start.len()..s.len() - 1]);
+            break;
+          }
+        }
+        let s = data.unwrap_or(&s);
+        s.parse::<u64>().map($kind)
+      }
+    }
+
+    fn deserialize<'de, D>(deserializer: D) -> Result<$kind, D::Error>
       where D: Deserializer<'de>
     {
       let s = String::deserialize(deserializer)?;
-      let mut data = None;
-      for start in $starts {
-        if s.starts_with(start) && s.ends_with('>') {
-          data = Some(&s[start.len()..s.len() - 1]);
-          break;
-        }
-      }
-      let s = data.unwrap_or(&s);
-      s.parse::<u64>().map($kind).map_err(|e| de::Error::custom(&format!($message, e)))
+      $name::parse(&s).map_err(|e| de::Error::custom(&format!($message, e)))
     }
 
     impl Deref for $name {
